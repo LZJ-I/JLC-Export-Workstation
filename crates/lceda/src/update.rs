14b63a8ea431f750d7fbe8308df7a4d6075ae6d1
LZJ-I <liu_zhen_jie@126.com>
2026-09-08 17:20:38 +0800

    0.8.0：简介模板、设置落盘，Designator 用立创位号前缀。

--- crates/lceda/src/update.rs
+++ crates/lceda/src/update.rs
@@ -407,7 +407,7 @@ mod tests {
         assert!(parse_version("0.5.2").unwrap() > parse_version("0.5.1").unwrap());
         assert!(parse_version("0.5.3").unwrap() > parse_version("0.5.2").unwrap());
         assert!(parse_version("0.6.0").unwrap() > parse_version("0.5.3").unwrap());
-        assert!(parse_version("0.7.0").unwrap() > parse_version("0.6.0").unwrap());
+        assert!(parse_version("0.8.0").unwrap() > parse_version("0.7.0").unwrap());
     }
 
     #[test]
