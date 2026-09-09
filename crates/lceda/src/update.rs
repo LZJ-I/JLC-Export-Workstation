754a4c0e697ed540b6aa0a8f69a0d79802250ce8
LZJ-I <liu_zhen_jie@126.com>
2026-09-09 09:27:13 +0800

    0.8.3：AD 位号不再被型号顶掉。

--- crates/lceda/src/update.rs
+++ crates/lceda/src/update.rs
@@ -410,6 +410,7 @@ mod tests {
         assert!(parse_version("0.8.0").unwrap() > parse_version("0.7.0").unwrap());
         assert!(parse_version("0.8.1").unwrap() > parse_version("0.8.0").unwrap());
         assert!(parse_version("0.8.2").unwrap() > parse_version("0.8.1").unwrap());
+        assert!(parse_version("0.8.3").unwrap() > parse_version("0.8.2").unwrap());
     }
 
     #[test]
